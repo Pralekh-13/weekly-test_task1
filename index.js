c824eb6cb9b42c7d4f2cb426844412221c1dc0e1
PRALEKH RAMBHAD <106136815+Pralekh-13@users.noreply.github.com>
2023-05-29 21:01:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let fruits = ["apple", "banana", "orange", "grape"];
+fruits.push("kiwi");
+fruits.pop();
+fruits.unshift("lemon");
+fruits.shift();
+let index = fruits.indexOf("orange");
+console.log(index);
+let citrus = fruits.slice(1, 3);
+console.log(citrus);
+
