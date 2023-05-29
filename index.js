02901af243b4e78367f313ca1f7763d6f9d3d4c2
PRALEKH RAMBHAD <106136815+Pralekh-13@users.noreply.github.com>
2023-05-29 20:47:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 let fruits = ["apple", "banana", "orange", "grape"];
-
 fruits.push("kiwi");
-console.log(fruits);
-
 fruits.pop();
-console.log(fruits);
-
 fruits.unshift("lemon");
-console.log(fruits);
-
 fruits.shift();
-console.log(fruits);
-
 let index = fruits.indexOf("orange");
-console.log(index); 
-
+console.log(index);
 let citrus = fruits.slice(1, 3);
-console.log(citrus); 
+console.log(citrus);
